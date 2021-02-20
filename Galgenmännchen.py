1b2592be498669d87e35ba01b693d04508bfa97b
zaened <79367260+zaened@users.noreply.github.com>
2021-02-20 15:16:14 +0100

    Add files via upload

---
+++ Galgenmännchen.py
@@ -0,0 +1,132 @@
+# author: Zäned Pasagad
+# date: 10.September 2020
+
+import random
+
+words = ['TEST','ELEKTRONIK','KOPFHÖRER','STROM','COMPILER','DISPLAY','PROGRAMM','UNI','ROBOTER','CODE']
+l = len(words) - 1      # Länge der Liste words
+b = ''      # Zufälliges Wort
+c = ''      # Vergleichsvariable und Ersatz
+d = []
+e = 0       # Counter
+f = 0       # Länge des Wortes
+
+def choice():
+    global b,c,f
+    b = words[random.randint(0,l)]
+    c = list(len(b) * '_')
+    f = len(b)
+    print('Das gesuchte Wort lautet:'+str(c))
+    print('Raten sie einen Buchstaben!')
+    
+def draw():
+    if e == 1:
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('________')
+    elif e == 2:
+        print('')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('___|____')
+    elif e == 3:
+        print('  ___________')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('___|____')
+    elif e == 4:
+        print('   ___________')
+        print('   |         |')
+        print('   |         |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('___|____')
+    elif e == 5:
+        print('   ___________')
+        print('   |         |')
+        print('   |         |')
+        print('   |         O')
+        print('   |')
+        print('   |')
+        print('   |')
+        print('___|____')
+    elif e == 6:
+        print('   ___________')
+        print('   |         |')
+        print('   |         |')
+        print('   |         O')
+        print('   |         |')
+        print('   |')
+        print('   |')
+        print('___|____')
+    elif e == 7:
+        print('   ___________')
+        print('   |         |')
+        print('   |         |')
+        print('   |         O')
+        print('   |         |')
+        print('   |        / \ ')
+        print('   |')
+        print('___|____')
+    elif e == 8:
+        print('   ___________')
+        print('   |         |')
+        print('   |         |')
+        print('   |         O')
+        print('   |        \|/')
+        print('   |        / \ ')
+        print('   |')
+        print('___|____')
+
+def swap(search):
+    global b,c,e
+    if search not in d:
+        if search in b:
+            for i in range(0,f,1):
+                if search == b[i]:
+                    c[i] = search
+            print('Richtig geraten! Weiter so!')
+            print('Gesuchtes Wort'+str(c))
+        else:
+            print('Leider haben sie falsch geraten! Versuchen sie es nochmal!')
+            print('Gesuchtes Wort:'+str(c))
+            e += 1
+    else:
+        print('Dieser Buchstabe wurde schon erraten! Versuchen sie es nochmal!')
+        print('Gesuchtes Wort:'+str(c))
+        e += 1
+    d.append(search)
+
+print('Herzlich Willkommen! Sie spielen das Spiel : Galgenmännchen!')
+print('Regel: Alle Eingaben müssen in Buchstaben erfolgen und sie können nur ein Buchstaben nach dem anderen erraten!')
+print('Das Spiel wird solange fortgesetzt, bis das Galgenmännchen gezeichnet wurde.')
+choice()
+
+while e < 8:
+    swap(input())
+    draw()
+    if e == 8 or '_' not in c:
+        break
+
+if e == 8:
+    print('Sie haben verloren! Das gesuchte Wort lautet: '+b)
+    input()
+
+if '_' not in c:
+    print('Sie haben gewonnen! Das gesuchte Wort lautet: '+b)
+    input()
